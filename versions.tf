14b2fe14a3a340203abe794d5d5b38811c6faa56
Umar Arafath <umararafath@opstimus.com>
2026-01-29 21:33:52 +0530

    DEVOPS-399: Upgrade-aws-provider-version

--- versions.tf
+++ versions.tf
@@ -3,12 +3,12 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = ">= 1.22.0"
+      version = ">= 2.5.0"
     }
 
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = ">= 6.0"
     }
 
     random = {
